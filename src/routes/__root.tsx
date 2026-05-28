6d6d3dd1b9e493c3aa05374c6a431975f9b98d1b
Chinaza Ozomagbo <114517797+Nazacyn@users.noreply.github.com>
2026-05-28 09:45:55 +0100

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -104,10 +104,7 @@ function RootShell({ children }: { children: React.ReactNode }) {
       <head>
         <HeadContent />
       </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
+      <body> {children} <ScrollRestoration /> <Scripts /> <script type="text/javascript" dangerouslySetInnerHTML={{ __html: ` (function(d, t) { var v = d.createElement(t), s = d.getElementsByTagName(t)[0]; v.onload = function() { window.voiceflow.chat.load({ verify: { projectID: '6a10ad0b5e0209f1acec04f6' }, url: 'https://general-runtime.voiceflow.com', voice: { url: "https://runtime-api.voiceflow.com" } }); } v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s); })(document, 'script'); `, }} /> </body>
     </html>
   );
 }
